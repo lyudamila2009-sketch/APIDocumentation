dfe115b37051cb549e455086bbc0eebe57ea7f59
Ludmila Gordeeva <lyuda.mila2009@email.com>
2026-07-25 12:27:24 +0300

    Add doc comments to todo models

--- internal/models/todo.go
+++ internal/models/todo.go
@@ -2,6 +2,7 @@ package models
 
 import "time"
 
+// Todo представляет задачу пользователя.
 type Todo struct {
 	ID          int       `json:"id"`
 	UserID      int       `json:"user_id"`
@@ -12,6 +13,7 @@ type Todo struct {
 	UpdatedAt   time.Time `json:"updated_at"`
 }
 
+// CreateTodoRequest содержит данные для создания новой задачи.
 type CreateTodoRequest struct {
 	UserID      int    `json:"user_id" validate:"required"`
 	Title       string `json:"title" validate:"required,max=200"`
@@ -19,6 +21,8 @@ type CreateTodoRequest struct {
 	Done        bool   `json:"done"`
 }
 
+// UpdateTodoRequest содержит данные для частичного обновления задачи.
+// Все поля опциональны: nil означает «не изменять».
 type UpdateTodoRequest struct {
 	Title       *string `json:"title" validate:"omitempty,max=200"`
 	Description *string `json:"description" validate:"omitempty,max=1000"`
